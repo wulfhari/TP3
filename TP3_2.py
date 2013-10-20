b8f7614c8c687576fa27f7c0182f8fefbc53aa8f
Zotol <admin@MacBook-Pro-de-Admin-2.local>
2013-10-20 14:31:24 -0400

    modif test_14h31

--- TP3_2.py
+++ TP3_2.py
@@ -38,7 +38,6 @@ def diff_symetrique(s1, s2):
     from TP3_1 import intersection
     """Commentaires ?"""
     uni = union(s1,s2)
-    print uni
     inter = intersection(s1,s2)
     
     for i in inter:       
@@ -56,10 +55,10 @@ def retrait(e, s):
 from TP3_1 import ajout, appartient, difference, intersection
 
 def test_appartient():
-    e = "Quebec"
-    s = ["a","b","t","allo","stop","clair","Quebec",2,3,54,7,5.4,6.7,9.8]
+    e = "Quebec""
+    s = ["a","b","t","allo","stop","clair","Quebec",2,3,54,7,5.4,6.7,9.8
     assert(True == appartient(e, s))
-    
+         
     e = 900
     s = ["a","b","t","allo","stop","clair","Quebec",2,3,54,7,5.4,6.7,9.8]
     assert(False == appartient(e, s))
@@ -67,17 +66,18 @@ def test_appartient():
 def test_intersection():
     s1 = ["a","b","t","allo","stop","clair","Quebec",2,3,54,7,5.4,6.7,9.8]
     s2 = ["a","e","f","123","go","clair","Quebec",1,4,78,7,5.3,6.7,9.8]
-    assert(['a', 'clair', 'Quebec', 7, 6.7, 9.8] == intersection(s1, s2)
-           
+    assert (["a", "clair", "Quebec", 7, 6.7, 9.8] == intersection(s1, s2))
+
 def test_difference():
     s1 = ["a","b","t","allo","stop","clair","Quebec",2,3,54,7,5.4,6.7,9.8]
     s2 = ["a","e","f","123","go","clair","Quebec",1,4,78,7,5.3,6.7,9.8]
-    assert(["b","t","allo","stop",2,3,54,5.4] == difference(s1, s2))
-           
+    assert (["b","t","allo","stop",2,3,54,5.4] == difference(s1, s2))
+
 def test_ajout():
     e = "Volga"
     s = ["a","b","t","allo","stop","clair","Quebec",2,3,54,7,5.4,6.7,9.8]
     assert(["a","b","t","allo","stop","clair","Quebec",2,3,54,7,5.4,6.7,9.8,"Volga"] == ajout(e, s))
+
            
 if __name__ == "__main__":
     test_appartient()
